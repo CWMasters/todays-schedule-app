76fd535eb85de2075830aa1c720c47cc24a02063
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-01-23 09:30:11 -0700

    added textArea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,91 @@
 let currentDay = document.querySelector("#currentDay");
 let container = document.querySelector(".container")
+let textArea = document.querySelector(".textarea")
 
+var tasks= [];
 
 // var moment = require('moment'); // require
 
 // moment().format(); MOMENT FORMAT
 
 
-
-
-
 // displaying time/date under header
-let rightNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); {
-    console.log(rightNow);
-    currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-}; 
+let rightNow = moment().format("dddd, MMMM Do YYYY"); {
+    currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
+     
+    // TODO UPDATE EVERY #) MINS FOR TASKS
+};
 
 
 
-    
+
+// // create task list to audit for color changes      
+// var createTask = function(textArea, taskTime) {
+//     // create element that make up task
+
+//     var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskTime);
+
+//     var taskP = $("<p>").addClass("m-1").text(textArea);
+
+//     // append spand and p to parent 
+//     taskli.append(taskSpan, taskP)
+
+//     // check due date
+//     auditTask(taskLi);
+
+//     // append to page
+//     $("#list-" + taskList).append(taskLi);
+  
+//   };
+
+//   var loadTasks = function() {
+//       tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//       // if nothing in local storage, create a new object to track all task status arrays
+//       if (!tasks) {
+//           tasks = {
+//               textarea: [],
+//           }
+//       };
+
+//       // loop over object proprties
+//       $.each(tasks, function(list, arr) {
+//         console.log(list, arr);
+//         // then loop over sub-array
+//         arr.forEach(function(task) {
+//           createTask(task.text, task.date, list);
+//         });
+//       });
+//   };
+
+//   var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+  
+  
+  
+  
+  
+//     // get and convert the due date
+//   var auditTask = function(taskEl) {
       
+//       //convert to moment object at 5:00pm
+//       var time = moment(date, "L").set("hour", 17);
     
-
-
+//       // remove any old classes from the element
+//       $(taskEl). removeClass("list-group-item-warning list-group-item-danger");
+    
+//       // apply new class if task is near/over due date
+//       if (moment().isAfter(time)) {
+//         $(taskEl).addClass("list-group-item-danger");
+//       }
+//       else if (Math.abs(moment().diff(time, "days")) <= 2) {
+//         $(taskEl).addClass("list-group-item-warning");
+//       }
+//     };
+    
+  
+  
 
 
 
@@ -37,41 +100,36 @@ let rightNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); {
 
 
 
-// from taskmaster
-$(".list-group").on("click", "p", function() {
-    // get current text of p element
-    var text = $(this)
-      .text()
-      .trim();
-  
-    // replace current p element with textarea
-    var textInput = $("<textarea>")
-      .addClass("form-control")
-      .val(text);
-    $(this).replaceWith(textInput);
-  
-  // focus new element
-  textInput.trigger("focus");
-  });
 
 
-  // get and convert the due date
-var auditTask = function(taskEl) {
-    // get date from task element
-    var date = $(taskEl).find("span").text().trim();
-    
-    //convert to moment object at 5:00pm
-    var time = moment(date, "L").set("hour", 17);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // // from taskmaster
+// $(".list-group").on("click", "p", function() {
+//     // get current text of p element
+//     var text = $(this)
+//       .text()
+//       .trim();
   
-    // remove any old classes from the element
-    $(taskEl). removeClass("list-group-item-warning list-group-item-danger");
+//     // replace current p element with textarea
+//     var textInput = $("<textarea>")
+//       .addClass("form-control")
+//       .val(text);
+//     $(this).replaceWith(textInput);
   
-    // apply new class if task is near/over due date
-    if (moment().isAfter(time)) {
-      $(taskEl).addClass("list-group-item-danger");
-    }
-    else if (Math.abs(moment().diff(time, "days")) <= 2) {
-      $(taskEl).addClass("list-group-item-warning");
-    }
-  };
-  
+//   // focus new element
+//   textInput.trigger("focus");
+//   });
